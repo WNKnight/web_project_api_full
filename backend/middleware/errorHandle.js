3b97414ab336a8af09cc668d92aa8487dc4db8d0
Aluno TripleTen <wesleyalmada@yahoo.com.br>
2024-05-18 21:13:38 -0300

    Atualização das funções de tratamento de erros para simplificar e padronizar o código

--- backend/middleware/errorHandle.js
+++ backend/middleware/errorHandle.js
@@ -5,37 +5,37 @@ const ERROR_NOT_FOUND = 404;
 const ERROR_DEFAULT = 500;
 
 const handleInvalidData = (res, message) => {
-  res.status(ERROR_INVALID_DATA).json({ message });
+  res.status(ERROR_INVALID_DATA).json({ message: message || 'Dados Invalidos' });
 };
 
-const handleUnauthorizedError = (res) => {
-  res.status(ERROR_UNAUTHORIZED).json({ message: 'Autorização necessária' });
-};
-
-const handleNotFoundError = (res, message) => {
-  res.status(ERROR_NOT_FOUND).json({ message });
+const handleUnauthorizedError = (res, message) => {
+  res.status(ERROR_UNAUTHORIZED).json({ message: message || 'Autorização necessária' });
 };
 
 const handleForbiddenError = (res, message) => {
-  res.status(ERROR_FORBIDDEN).json({ message });
+  res.status(ERROR_FORBIDDEN).json({ message: message || 'Acesso proibido' });
 };
 
-const handleDefaultError = (res) => {
-  res.status(ERROR_DEFAULT).json({ message: 'Ocorreu um erro no servidor' });
+const handleNotFoundError = (res, message) => {
+  res.status(ERROR_NOT_FOUND).json({ message: message || 'Recurso solicitado não encontrado' });
+};
+
+const handleDefaultError = (res, message) => {
+  res.status(ERROR_DEFAULT).json({ message: message || 'Ocorreu um erro no servidor' });
 };
 
 module.exports = (err, req, res, next) => {
   if (err.statusCode === ERROR_INVALID_DATA) {
-    return handleInvalidData(res, err.message || 'Dados fornecidos inválidos');
+    return handleInvalidData(res, err.message);
   }
-  if (err.name === 'UnauthorizedError') {
-    return handleUnauthorizedError(res);
+  if (err.statusCode === ERROR_UNAUTHORIZED) {
+    return handleUnauthorizedError(res, err.message);
   }
   if (err.statusCode === ERROR_FORBIDDEN) {
-    return handleForbiddenError(res, err.message || 'Acesso proibido');
+    return handleForbiddenError(res, err.message);
   }
   if (err.statusCode === ERROR_NOT_FOUND) {
-    return handleNotFoundError(res, err.message || 'Recurso solicitado não encontrado');
+    return handleNotFoundError(res, err.message);
   }
-  handleDefaultError(res);
+  handleDefaultError(res, err.message);
 };
